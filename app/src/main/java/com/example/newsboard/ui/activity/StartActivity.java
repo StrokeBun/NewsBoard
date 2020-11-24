33775b691775602f0a1cf2e95e4b4cd69a4237f2
StrokeBun <stroke0.0bun@gmail.com>
2020-11-24 15:58:03 +0800

    改为使用SharedPreferences存储token

--- app/src/main/java/com/example/newsboard/ui/activity/StartActivity.java
+++ app/src/main/java/com/example/newsboard/ui/activity/StartActivity.java
@@ -3,29 +3,22 @@ package com.example.newsboard.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+import android.preference.PreferenceManager;
 
 import com.example.newsboard.R;
+import com.example.newsboard.util.TokenUtils;
 
-import java.util.Calendar;
 
 public class StartActivity extends AppCompatActivity {
 
-    private static final String GOOD_MORNING = "早上好, 打工人";
-    private static final String GOOD_NOON = "中午好，打工人";
-    private static final String GOOD_AFTERNOON = "下午好，打工人";
-    private static final String GOOD_EVENING = "晚上好, 打工人";
-
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        textView = findViewById(R.id.start_page_text);
-        setText();
+        initApp();
 
         new Thread(() -> {
             try {
@@ -37,18 +30,9 @@ public class StartActivity extends AppCompatActivity {
         }).start();
     }
 
-    private void setText() {
-        Calendar cal = Calendar.getInstance();
-        int hour = cal.get(Calendar.HOUR_OF_DAY);
-        Log.e("hour", String.valueOf(hour));
-        if (hour >= 14 && hour <= 17) {
-            textView.setText(GOOD_AFTERNOON);
-        } else if (hour >= 5 && hour <= 9) {
-            textView.setText(GOOD_MORNING);
-        } else if (hour >= 10 && hour <= 13) {
-            textView.setText(GOOD_NOON);
-        } else {
-            textView.setText(GOOD_EVENING);
-        }
+    private void initApp() {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        TokenUtils.initTokenUtils(pref);
     }
+
 }
--- app/src/main/java/com/example/newsboard/util/TokenUtils.java
+++ app/src/main/java/com/example/newsboard/util/TokenUtils.java
@@ -1,6 +1,11 @@
 package com.example.newsboard.util;
 
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.example.newsboard.ui.activity.LoginActivity;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,15 +21,15 @@ import java.util.Map;
  */
 public final class TokenUtils {
 
-    // http响应JSON中token对应的key
-    private static final String RESPONSE_TOKEN_KEY = "token";
+    private static SharedPreferences pref;
+    // token对应的key
+    private static final String TOKEN_KEY = "token";
     // http请求头中权限对应的key
     private static final String REQUEST_HEADER_AUTHORIZATION_KEY = "Authorization";
     // http请求头中token前缀
     private static final String AUTHORIZATION_TOKEN_PREFIX = "Bearer ";
     // 本地未存储token的默认值
     private static final String NO_TOKEN = "";
-    private static String token = NO_TOKEN;
 
     /**
      * 返回使用token进行权限识别的http header，返回类型为Map<String, String>
@@ -32,7 +37,7 @@ public final class TokenUtils {
     public static Map<String, String> getAuthorizationHeader() {
         return new HashMap<String, String>() {
             {
-                String auth = AUTHORIZATION_TOKEN_PREFIX + token;
+                String auth = AUTHORIZATION_TOKEN_PREFIX + getToken();
                 put(REQUEST_HEADER_AUTHORIZATION_KEY, auth);
             }
         };
@@ -42,7 +47,7 @@ public final class TokenUtils {
      * 返回是否已经缓存了token
      */
     public static boolean isEmptyToken() {
-        return token.equals(NO_TOKEN);
+        return getToken().equals(NO_TOKEN);
     }
 
     /**
@@ -51,16 +56,30 @@ public final class TokenUtils {
      */
     public static void setTokenFromResponse(String response) throws JSONException {
         JSONObject jsonObject = new JSONObject(response);
-        token = (String) jsonObject.get(RESPONSE_TOKEN_KEY);
+        String token = (String) jsonObject.get(TOKEN_KEY);
+        setToken(token);
     }
 
     /**
      * 清除已经缓存的token，用户退出登录后调用
      */
     public static void clearToken() {
-        token = NO_TOKEN;
+        setToken(NO_TOKEN);
     }
 
+    public static void initTokenUtils(SharedPreferences aPref) {
+        pref = aPref;
+    }
+
+    private static String getToken() {
+        return pref.getString(TOKEN_KEY, NO_TOKEN);
+    }
+
+    private static void setToken(String token) {
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(TOKEN_KEY, token);
+        editor.apply();
+    }
 
     private TokenUtils() {
         throw new UnsupportedOperationException("TokenUtils cant be constructed");
